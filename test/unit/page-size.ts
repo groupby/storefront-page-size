2737839a17a69a143526203e8be1d9a8c45213ff
Ben Teichman <ben.l.teichman@gmail.com>
2017-06-07 20:39:03 -0400

    Add sinon-chai, wallaby.js support (#2)

--- test/unit/page-size.ts
+++ test/unit/page-size.ts
@@ -2,16 +2,40 @@ import { Events } from '@storefront/core';
 import PageSize from '../../src/page-size';
 import suite from './_suite';
 
+const PAGE_SIZES = [10, 20, 30];
+
 suite('PageSize', ({ expect, spy, stub }) => {
   let pageSize: PageSize;
   let selectPageSizesStub: sinon.SinonStub;
 
   beforeEach(() => {
-    PageSize.prototype.flux = <any>{ store: { getState: () => ({ data: { page: { sizes: {} } } }) } };
+    PageSize.prototype.flux = <any>{ store: { getState: () => ({ data: { page: { sizes: PAGE_SIZES } } }) } };
     selectPageSizesStub = stub(PageSize.prototype, 'selectPageSizes');
     pageSize = new PageSize();
   });
   afterEach(() => delete PageSize.prototype.flux);
+
+  describe('constructor()', () => {
+    describe('state', () => {
+      it('should set initial value', () => {
+        const sizes = [15, 30, 50];
+
+        expect(selectPageSizesStub).to.be.calledWith(PAGE_SIZES);
+      });
+
+      describe('onSelect()', () => {
+        it('should call flux.resize()', () => {
+          const resize = spy();
+          pageSize.flux = <any>{ resize };
+          pageSize.state.pageSizes = <any[]>[{ value: 20 }, { value: 40 }, { value: 80 }];
+
+          pageSize.state.onSelect(1);
+
+          expect(resize).to.be.calledWith(40);
+        });
+      });
+    });
+  });
 
   describe('init()', () => {
     it('should listen for PAGE_SIZE_UPDATED', () => {
@@ -20,7 +44,7 @@ suite('PageSize', ({ expect, spy, stub }) => {
 
       pageSize.init();
 
-      expect(on.calledWith(Events.PAGE_SIZE_UPDATED, pageSize.updatePageSizes)).to.be.true;
+      expect(on).to.be.calledWith(Events.PAGE_SIZE_UPDATED, pageSize.updatePageSizes);
     });
   });
 
@@ -33,8 +57,8 @@ suite('PageSize', ({ expect, spy, stub }) => {
 
       pageSize.updatePageSizes(state);
 
-      expect(selectPageSizes.calledWith(state)).to.be.true;
-      expect(set.calledWith({ pageSizes: selected }));
+      expect(selectPageSizes).to.be.calledWith(state);
+      expect(set).to.be.calledWith({ pageSizes: selected });
     });
   });
 
