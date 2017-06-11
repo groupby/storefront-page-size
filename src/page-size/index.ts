1650afdeccdf8120a8269a3c3b92526be4f7209f
Ben Teichman <ben.l.teichman@gmail.com>
2017-06-11 13:25:44 -0400

    feat(storefront): initial release

---
+++ src/page-size/index.ts
@@ -0,0 +1,42 @@
+import { alias, tag, Events, Selectors, Store, Tag } from '@storefront/core';
+
+@alias('pageSize')
+@tag('gb-page-size', require('./index.html'))
+class PageSize {
+
+  state: PageSize.State = {
+    pageSizes: this.selectPageSizes(this.flux.store.getState().data.page.sizes),
+    onSelect: (index) => this.flux.resize(this.state.pageSizes[index].value)
+  };
+
+  init() {
+    this.flux.on(Events.PAGE_SIZE_UPDATED, this.updatePageSizes);
+  }
+
+  updatePageSizes = (state: Store.SelectableList<number>) =>
+    this.set({ pageSizes: this.selectPageSizes(state) })
+
+  selectPageSizes(state: Store.SelectableList<number>) {
+    return state.items.map((pageSize, index) => ({
+      value: pageSize,
+      label: pageSize,
+      selected: index === state.selected
+    }));
+  }
+}
+
+interface PageSize extends Tag<any, PageSize.State> { }
+namespace PageSize {
+  export interface State {
+    pageSizes: Option[];
+    onSelect(index: number): void;
+  }
+
+  export interface Option {
+    value: number;
+    label: number;
+    selected?: boolean;
+  }
+}
+
+export default PageSize;
---
+++ test/unit/page-size.ts
@@ -0,0 +1,83 @@
+import { Events } from '@storefront/core';
+import PageSize from '../../src/page-size';
+import suite from './_suite';
+
+const PAGE_SIZES = [10, 20, 30];
+
+suite('PageSize', ({ expect, spy, stub }) => {
+  let pageSize: PageSize;
+  let selectPageSizesStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    PageSize.prototype.flux = <any>{ store: { getState: () => ({ data: { page: { sizes: PAGE_SIZES } } }) } };
+    selectPageSizesStub = stub(PageSize.prototype, 'selectPageSizes');
+    pageSize = new PageSize();
+  });
+  afterEach(() => delete PageSize.prototype.flux);
+
+  describe('constructor()', () => {
+    describe('state', () => {
+      it('should set initial value', () => {
+        const sizes = [15, 30, 50];
+
+        expect(selectPageSizesStub).to.be.calledWith(PAGE_SIZES);
+      });
+
+      describe('onSelect()', () => {
+        it('should call flux.resize()', () => {
+          const resize = spy();
+          pageSize.flux = <any>{ resize };
+          pageSize.state.pageSizes = <any[]>[{ value: 20 }, { value: 40 }, { value: 80 }];
+
+          pageSize.state.onSelect(1);
+
+          expect(resize).to.be.calledWith(40);
+        });
+      });
+    });
+  });
+
+  describe('init()', () => {
+    it('should listen for PAGE_SIZE_UPDATED', () => {
+      const on = spy();
+      pageSize.flux = <any>{ on };
+
+      pageSize.init();
+
+      expect(on).to.be.calledWith(Events.PAGE_SIZE_UPDATED, pageSize.updatePageSizes);
+    });
+  });
+
+  describe('updatePageSizes()', () => {
+    it('should set pageSizes', () => {
+      const state: any = { a: 'b' };
+      const selected = ['c', 'd'];
+      const selectPageSizes = pageSize.selectPageSizes = spy(() => selected);
+      const set = pageSize.set = spy();
+
+      pageSize.updatePageSizes(state);
+
+      expect(selectPageSizes).to.be.calledWith(state);
+      expect(set).to.be.calledWith({ pageSizes: selected });
+    });
+  });
+
+  describe('selectPageSizes()', () => {
+    beforeEach(() => selectPageSizesStub.restore());
+
+    it('should remap page sizes to options', () => {
+      const state: any = {
+        items: [12, 14, 16],
+        selected: 2
+      };
+
+      const pageSizes = pageSize.selectPageSizes(state);
+
+      expect(pageSizes).to.eql([
+        { value: 12, label: 12, selected: false },
+        { value: 14, label: 14, selected: false },
+        { value: 16, label: 16, selected: true }
+      ]);
+    });
+  });
+});
