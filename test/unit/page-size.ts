2c3b61f15a396a04ce0346176be8de63cf3c2bab
Filip-F <f.francet@hotmail.com>
2017-10-24 12:07:26 -0400

    refactor(BBD-799): Simplify tag selectors (#15)

--- test/unit/page-size.ts
+++ test/unit/page-size.ts
@@ -3,17 +3,16 @@ import PageSize from '../../src/page-size';
 import suite from './_suite';
 
 const PAGE_SIZES = [10, 20, 30];
-const STATE = { y: 'z' };
 
 suite('PageSize', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlias }) => {
   let pageSize: PageSize;
   let selectPageSizesStub: sinon.SinonStub;
-  let pageSizesSelector: sinon.SinonStub;
+  let select: sinon.SinonSpy;
 
   beforeEach(() => {
-    PageSize.prototype.flux = <any>{ store: { getState: () => STATE } };
+    PageSize.prototype.flux = <any>{};
     selectPageSizesStub = stub(PageSize.prototype, 'selectPageSizes');
-    pageSizesSelector = stub(Selectors, 'pageSizes').returns(PAGE_SIZES);
+    select = PageSize.prototype.select = spy(() => PAGE_SIZES);
     pageSize = new PageSize();
   });
   afterEach(() => delete PageSize.prototype.flux);
@@ -26,7 +25,7 @@ suite('PageSize', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlia
       it('should set initial value', () => {
         const sizes = [15, 30, 50];
 
-        expect(pageSizesSelector).to.be.calledWith(STATE);
+        expect(select).to.be.calledWith(Selectors.pageSizes);
         expect(selectPageSizesStub).to.be.calledWith(PAGE_SIZES);
       });
 
